3131aec2e2fd99fea5d64b2b0526be0ca4bc1f80
Kamil Górny <kamil.gorny99@gmail.com>
2024-09-27 19:35:45 +0200

    feat: Add auth support to swagger

--- DropSpot.Api/Program.cs
+++ DropSpot.Api/Program.cs
@@ -3,21 +3,40 @@ using DropSpot.Application.Extensions;
 using DropSpot.Domain.Entities;
 using DropSpot.Infrastructure.Extensions;
 using DropSpot.Infrastructure.Seeders;
+using Microsoft.OpenApi.Models;
 using Serilog;
 using Serilog.Events;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.AddSecurityDefinition("bearerAuth", new OpenApiSecurityScheme()
+    {
+        Type = SecuritySchemeType.Http,
+        Scheme = "Bearer",
+    });
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+    {
+        {
+            new OpenApiSecurityScheme()
+            {
+                Reference = new OpenApiReference()
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "bearerAuth"
+                }
+            },
+            []
+        }
+    });
+});
 builder.Services.AddEndpointsApiExplorer();
 
 builder.Services.AddInfrastructure(builder.Configuration);
 builder.Services.AddApplication();
 builder.Services.AddScoped<ErrorHandlingMiddleware>();
-builder.Host.UseSerilog((context, configuration) =>
-{
-    configuration.ReadFrom.Configuration(context.Configuration);
-});
+builder.Host.UseSerilog((context, configuration) => { configuration.ReadFrom.Configuration(context.Configuration); });
 
 
 var app = builder.Build();
@@ -27,6 +46,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
 app.UseSerilogRequestLogging();
 app.UseMiddleware<ErrorHandlingMiddleware>();
 
@@ -40,7 +60,6 @@ app.MapGroup("api/identity").MapIdentityApi<User>();
 app.UseCors(x => x
     .AllowAnyMethod()
     .AllowAnyHeader()
-    .SetIsOriginAllowed(origin => true) 
+    .SetIsOriginAllowed(origin => true)
     .AllowCredentials());
 app.Run();
-
