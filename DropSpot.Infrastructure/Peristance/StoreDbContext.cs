e9908dc8c76a51bb592008b37358baf4a689fecb
Kamil Górny <kamil.gorny99@gmail.com>
2024-10-08 21:27:30 +0200

    feat: Add Orders db set

--- DropSpot.Infrastructure/Peristance/StoreDbContext.cs
+++ DropSpot.Infrastructure/Peristance/StoreDbContext.cs
@@ -7,6 +7,7 @@ namespace DropSpot.Infrastructure.Peristance;
 internal class StoreDbContext(DbContextOptions options) : IdentityDbContext<User>(options)
 {
     internal DbSet<Product> Products { get; set; }
+    internal DbSet<Order> Orders { get; set; }
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
